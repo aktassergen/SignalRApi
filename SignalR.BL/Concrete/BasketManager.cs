0e5309eef76b19c4cfb743d074fd94b8ecff3861
sergen aktaş <srgn_akts@hotmail.com>
2024-04-21 03:39:51 +0300

    dynamic rezervation added with signalr

--- SignalR.BL/Concrete/BasketManager.cs
+++ SignalR.BL/Concrete/BasketManager.cs
@@ -25,17 +25,17 @@ namespace SignalR.BL.Concrete
 
         public void TAdd(Basket entity)
         {
-            throw new NotImplementedException();
+            _basketDal.Add(entity);
         }
 
         public void TDelete(Basket entity)
         {
-            throw new NotImplementedException();
+            _basketDal.Delete(entity);
         }
 
         public Basket TGetById(int id)
         {
-            throw new NotImplementedException();
+            return _basketDal.GetById(id);
         }
 
         public List<Basket> TGetListAll()
--- SignalRApi/Controllers/BasketController.cs
+++ SignalRApi/Controllers/BasketController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SignalR.BL.Abstract;
 using SignalR.DAL.Concrete;
+using SignalR.Dto.BasketDto;
+using SignalR.Entity.Entities;
 using SignalRApi.Models;
 
 namespace SignalRApi.Controllers
@@ -20,26 +21,47 @@ namespace SignalRApi.Controllers
         [HttpGet]
         public IActionResult GetBasketByMenuTableId(int id)
         {
-            var values=_basketService.TGetBasketByMenuTableNumber(id);
+            var values = _basketService.TGetBasketByMenuTableNumber(id);
             return Ok(values);
         }
         [HttpGet("BasketListByMenuTableWithProductName")]
         public IActionResult BasketListByMenuTableWithProductName(int id)
         {
-            using var context=new SignalRContext();//solid biraz ezilmek zorunda kaldı
-            var values= context.Baskets.Include(x => x.Product).Where(y => y.MenuTableId == id)
+            using var context = new SignalRContext();//solid biraz ezilmek zorunda kaldı
+            var values = context.Baskets.Include(x => x.Product).Where(y => y.MenuTableId == id)
                 .Select(z => new ResultBasketListWithProducts
-            {
-                    BasketId=z.BasketId,
-                    Count=z.Count,
-                    MenuTableId=z.MenuTableId,
-                    Price=z.Price,
-                    ProductId=z.ProductId,
+                {
+                    BasketId = z.BasketId,
+                    Count = z.Count,
+                    MenuTableId = z.MenuTableId,
+                    Price = z.Price,
+                    ProductId = z.ProductId,
                     TotalPrice = z.TotalPrice,
                     ProductName = z.Product.ProductName,
 
                 }).ToList();
             return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateBasket(CreateBasketDto createBasketDto)
+        {
+            using var context = new SignalRContext();
+            _basketService.TAdd(new Basket()
+            {
+                ProductId = createBasketDto.ProductId,
+                Count = 1,
+                MenuTableId = 4,
+                Price = (decimal)context.Products.Where(x => x.ProductId == createBasketDto.ProductId).Select(y => y.ProductPrice).FirstOrDefault(),
+                TotalPrice = 0
+            });
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBasket(int id)
+        {
+            var value = _basketService.TGetById(id);
+            _basketService.TDelete(value);
+            return Ok("Seçilen Ürün Sepetten Silindi");
         }
     }
 }
